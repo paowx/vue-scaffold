b0e3bd3f3b70a5010f5e39a9e2af587a87bf2286
lijiamei <1052887510@qq.com>
2019-09-18 14:22:17 +0800

    change

--- src/components/loadMore.vue
+++ src/components/loadMore.vue
@@ -1,9 +1,14 @@
 <template>
   <!--上拉加载更多功能-->
 <div class="load-more-warp">
-  <p>body.clientHeight: {{clientHeight1}}, documentElement.clientHeight: {{clientHeight2}}</p>
-  <p>body.scrollHeight: {{scrollHeight1}}, documentElement.scrollHeight: {{scrollHeight2}}</p>
-  <p>body.scrollTop: {{scrollTop1}}, documentElement.scrollTop: {{scrollTop2}}</p>
+  <div class="fiexd-div">
+    <p>body.clientHeight: {{clientHeight1}}, documentElement.clientHeight: {{clientHeight2}}</p>
+    <p>body.offsetHeight: {{offsetHeight1}}, documentElement.offsetHeight: {{offsetHeight2}}</p>
+    <p>innerHeight: {{innerHeight}}</p>
+    <p>body.scrollHeight: {{scrollHeight1}}, documentElement.scrollHeight: {{scrollHeight2}}</p>
+    <p>pageYOffset: {{pageYOffset}}</p>
+    <p>body.scrollTop: {{scrollTop1}}, documentElement.scrollTop: {{scrollTop2}}</p>
+  </div>
   <div class="list">
     <div class="item" v-for="item in list" :key="item">{{item}}</div>
   </div>
@@ -18,8 +23,12 @@ export default {
       clientHeight2: 0,
       scrollHeight1: 0,
       scrollHeight2: 0,
+      pageYOffset: 0,
       scrollTop1: 0,
       scrollTop2: 0,
+      offsetHeight1: 0,
+      offsetHeight2: 0,
+      innerHeight: 0,
       list: [],
       index: 0
     }
@@ -30,12 +39,14 @@ export default {
     this.fetchData()
     setTimeout(() => {
       let offsetHeight = document.getElementsByClassName('load-more-warp')[0].offsetHeight
+      this.getOffsetHeight()
       console.log(offsetHeight, this.getClientHeight())
       if (offsetHeight < this.getClientHeight()) {
         this.fetchData()
       }
     }, 1500)
     window.onscroll = () => {
+      this.getOffsetHeight()
       console.log(this.getScrollTop(), this.getClientHeight(), this.getScrollHeight())
       if (this.getScrollTop() + this.getClientHeight() === this.getScrollHeight()) {
         this.fetchData()
@@ -49,7 +60,6 @@ export default {
      */
     getClientHeight() {
       let clientHeight = 0
-      // console.log('clientHeight::', document.body.clientHeight, document.documentElement.clientHeight)
       if (document.body.clientHeight && document.documentElement.clientHeight) {
         clientHeight = Math.min(document.body.clientHeight, document.documentElement.clientHeight)
       } else {
@@ -59,20 +69,24 @@ export default {
       this.clientHeight2 = document.documentElement.clientHeight
       return clientHeight
     },
+    getOffsetHeight() {
+      this.offsetHeight1 = document.body.offsetHeight
+      this.offsetHeight2 = document.documentElement.offsetHeight
+      this.innerHeight = window.innerHeight
+    },
     /**
      * 获取当前文档的完整高度
      * @returns {number}
      */
     getScrollHeight() {
       let scrollHeight = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight)
-      // console.log('scrollHeight::', document.body.scrollHeight, document.documentElement.scrollHeight)
       this.scrollHeight1 = document.body.scrollHeight
       this.scrollHeight2 = document.documentElement.scrollHeight
+      this.pageYOffset = window.pageYOffset
       return scrollHeight
     },
     getScrollTop() {
       let scrollTop = document.body.scrollTop || document.documentElement.scrollTop
-      // console.log('scrollTop::', document.body.scrollTop, document.documentElement.scrollTop)
       this.scrollTop1 = document.body.scrollTop
       this.scrollTop2 = document.documentElement.scrollTop
       return scrollTop
@@ -91,9 +105,20 @@ export default {
 
 <style lang="scss">
 .load-more-warp{
+  font-size: .7rem;
+  padding: 1rem;
+  .fiexd-div{
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    background: #ffffff;
+    padding: 1rem;
+    box-sizing: border-box;
+  }
   .list{
     .item{
-      padding: 0rem 1rem;
+      padding: 1rem 1rem;
       background: #4caf50;
       color: #ffffff;
       margin-top: .5rem;
